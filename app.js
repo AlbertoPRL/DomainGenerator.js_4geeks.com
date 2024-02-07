a1586c029392fd08cc2ebca0a1842ab4f4c3086d
AlbertoPRL <67210580+AlbertoPRL@users.noreply.github.com>
2024-02-06 21:32:38 -0500

    Final_Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const domainGenerator = () => {
+    const pronoun = ['the', 'our', 'one'];
+    const adj = ['great', 'big','amazing','thirsty','dizzy',];
+    const noun = ['jogger', 'racoon', 'elephant','ant','guy','dog','cat','pub'];
+    const domain = ['.com', '.es', '.org', '.us','.net',]
+
+    return `${pronoun[randomIndex(pronoun)]}${adj[randomIndex(adj)]}${noun[randomIndex(noun)]}${domain[randomIndex(domain)]}`;
+}
+
+const randomIndex = (array) => {
+    return Math.floor(Math.random() * array.length);
+}
+
+const askQuestion = () => {
+    rl.question("Type 'yes' to generate a random domain. Type 'no' to terminate: ", (userAnswer) => {
+        if (userAnswer.toLowerCase() === "yes") {
+            console.log(domainGenerator());
+            askQuestion();
+        } else {
+            rl.close();
+        }
+    });
+};
+
+function start(){
+    askQuestion();
+}
+
+start();
+
