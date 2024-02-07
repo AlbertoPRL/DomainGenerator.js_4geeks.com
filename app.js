55d673b4eee2be91a7e3c05cd2d957bc543aaa19
AlbertoPRL <67210580+AlbertoPRL@users.noreply.github.com>
2024-02-07 18:57:18 -0500

    minor_changes_2

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 function start(){
-    const readline = require('readline');
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-});
+  
     askQuestion();
 }
 
